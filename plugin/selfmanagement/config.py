bc580f8e6aec0c9fa5a0d561357508640ad93c93
KirbyScarlet <KirbySSU@outlook.com>
2023-08-15 15:49:19 +0800

    解决因ocr识别错误后，未处理队列导致图片任务堵塞

---
+++ plugin/selfmanagement/config.py
@@ -0,0 +1,12 @@
+from pydantic import BaseSettings
+
+from typing import Optional, Union, Tuple, Mapping, Dict
+from nonebot import get_driver
+
+class Config(BaseSettings):
+    self_management_suffix: list[str] = ["#!/",]
+
+    class Config:
+        extra = "ignore"
+
+self_management_config = Config.parse_obj(get_driver().config)
---
+++ plugin/selfmanagement/main.py
@@ -0,0 +1,4 @@
+#
+
+from nonebot import on_startswith
+
